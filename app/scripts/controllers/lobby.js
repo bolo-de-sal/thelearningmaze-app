8782796a5e7dceeb945352fb475b4c0add52b5da
ThiagoFerreiraS <thiago.ferreira@yopye.com.br>
2016-05-21 13:49:50 -0300

    Alterações nas dimenções do card de grupos.

--- app/scripts/controllers/lobby.js
+++ app/scripts/controllers/lobby.js
@@ -15,4 +15,11 @@ angular
     function LobbyController($location, $rootScope, GlobalMessageService) {
 
         console.log("--->LobbyController init");
+
+        var div = '<div class="col-md-4"><div class="card grupo"><p>P</p><p>POWERPUFF</p><p>Formação</p><p>Alunos: 4</p><div class="alunos"><div class="aluno"><img src="../images/pu.jpg" alt="Professor Utonio"><p>aluno1</p></div><div class="aluno"><img src="../images/pu.jpg" alt="Professor Utonio"><p>aluno2</p></div><div class="aluno"><img src="../images/pu.jpg" alt="Professor Utonio"><p>aluno3</p></div><div class="aluno"><img src="../images/pu.jpg" alt="Professor Utonio"><p>aluno4</p></div></div><p>Assunto</p></div></div>';
+
+        for(var i = 0; i < 100; i++){
+        	$('.grupos').append(div);
+        }
+
     }
